280cd598ae6f60c6f365b4877a0efe208b091599
o644l <oemerfaruk.durugoel@dkfz-heidelberg.de>
2025-08-27 14:27:03 +0200

    Add 175e DA5Segord0 trainer

--- nnunetv2/training/nnUNetTrainer/variants/lr_schedule/nnUNetTrainer_finetuning.py
+++ nnunetv2/training/nnUNetTrainer/variants/lr_schedule/nnUNetTrainer_finetuning.py
@@ -606,6 +606,14 @@ class nnUNetTrainer1e3_150e_DA5Segord0(nnUNetTrainerDualVal_DA5Segord0):
         self.initial_lr = 1e-3
         self.num_epochs = 150
 
+class nnUNetTrainer1e3_175e_DA5Segord0(nnUNetTrainerDualVal_DA5Segord0):
+    def __init__(self, plans: dict, configuration: str, fold: int, dataset_json: dict,
+                 device: torch.device = torch.device('cuda')):
+        super().__init__(plans, configuration, fold, dataset_json, device)
+
+        self.initial_lr = 1e-3
+        self.num_epochs = 175
+
 class nnUNetTrainer1e3_200e_DA5Segord0(nnUNetTrainerDualVal_DA5Segord0):
     def __init__(self, plans: dict, configuration: str, fold: int, dataset_json: dict,
                  device: torch.device = torch.device('cuda')):
